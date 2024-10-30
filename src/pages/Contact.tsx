081fd88bee19e3a71b02f5c8f114d920a1ba3eff
Valeriia Lyakisheva <valeriia.lyakisheva@bahag.com>
2024-10-30 10:04:25 +0100

    Products and Contact components created

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,9 @@
+const Contact: React.FC = () => {
+  return (
+    <>
+      <h1>Products List</h1>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Products.tsx
@@ -0,0 +1,9 @@
+const Products: React.FC = () => {
+  return (
+    <>
+      <h1>Products List</h1>
+    </>
+  );
+};
+
+export default Products;
